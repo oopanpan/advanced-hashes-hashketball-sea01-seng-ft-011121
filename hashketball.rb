8e74dd8f6ac4a9a7cd10e846173e0df9ad92f103
Kai Peng Li <oopanpan@gmail.com>
2020-12-26 06:33:01 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 def game_hash
   {
@@ -127,3 +128,154 @@ def game_hash
 end
 
 # Write code here
+def get_player_details
+  game_hash
+  new_hash = {}
+  
+end
+        
+
+def num_points_scored(input_name)
+  game_hash
+  score = 0
+  game_hash.each { |home_key, home_value|
+      home_value.each { |titles, player_details|
+        if titles == :players
+          #binding.pry
+          player_details.length.times { |i|
+            if player_details[i][:player_name] == input_name
+              #binding.pry
+              score = player_details[i][:points]
+              #binding.pry
+            else
+              next
+            end
+          }
+        else
+          next
+        end
+      }
+  }
+  score
+end
+
+def shoe_size(input_name)
+  game_hash
+  shoe = 0
+  game_hash.each { |home_key, home_value|
+      home_value.each { |titles, player_details|
+        if titles == :players
+          player_details.length.times { |i|
+            if player_details[i][:player_name] == input_name
+              shoe = player_details[i][:shoe]
+            else
+              next
+            end
+          }
+        else
+          next
+        end
+      }
+  }
+  shoe
+end
+
+def team_colors(input_name)
+  game_hash
+  final_color = []
+  game_hash.each { |home_key, home_value|
+    home_value.each { |titles, content|
+    if content == input_name
+      final_color = game_hash[home_key][:colors]
+      #binding.pry
+    else
+      next
+    end
+    }
+  }
+  final_color
+end
+
+def team_names
+  game_hash
+  fin_arr = []
+  game_hash.each { |home_key, home_value|
+    fin_arr << game_hash[home_key][:team_name]
+  }
+  fin_arr
+end
+
+def player_numbers(input_name)
+  game_hash
+  jersey = []
+  game_hash.each { |home_key, home_value|
+    #binding.pry
+    if home_value[:team_name] == input_name
+      home_value.each { |titles, player_details|
+        if titles == :players
+          player_details.length.times { |i|
+            jersey << player_details[i][:number]
+          }
+        end
+      }
+    else
+      next
+    end
+  }
+  jersey
+end
+
+def player_stats(input_name)
+  game_hash
+  fin_har = {}
+  game_hash.each { |home_key, home_value|
+      home_value.each { |titles, player_details|
+        if titles == :players
+          player_details.length.times { |i|
+            if player_details[i][:player_name] == input_name
+              fin_har = player_details[i]
+            else
+              next
+            end
+          }
+        else
+          next
+        end
+      }
+  }
+  fin_har
+end
+
+def big_shoe_rebounds
+  game_hash
+  shoe = 0
+  rebounds = 0
+  game_hash.each { |home_key, home_value|
+      home_value.each { |titles, player_details|
+        if titles == :players
+          player_details.length.times { |i|
+            if shoe < player_details[i][:shoe]
+              shoe = player_details[i][:shoe]
+            else
+              next
+            end
+          }
+        else
+          next
+        end
+        if titles == :players
+          player_details.length.times { |i|
+            if player_details[i][:shoe] == shoe
+              rebounds = player_details[i][:rebounds]
+              #binding.pry
+            else
+              next
+            end
+          }
+        else
+          next
+        end
+      }
+  }
+rebounds
+end
